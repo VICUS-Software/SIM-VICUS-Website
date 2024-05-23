71aed66a7b940bf29affb46fb072d6f074a1485f
Paul <72733450+paul1995tu@users.noreply.github.com>
2024-05-23 16:57:33 +0200

    test pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  site:"https://sime-vicus-homepage.github.io",
+  site:"https://vicus-software.github.io",
   vite: {
     plugins: [vanillaExtractPlugin()],
   },
