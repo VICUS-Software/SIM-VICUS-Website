61da2febdff37650e9c1c1a8001cb413706f478a
Paul <72733450+paul1995tu@users.noreply.github.com>
2024-05-25 11:54:48 +0200

    test pages

--- src/utils/githubPagesLinkHelper.ts
+++ src/utils/githubPagesLinkHelper.ts
@@ -1,6 +1,9 @@
 
 export function githubPagesLinkHelper(linkName:string):string{
     if (import.meta.env.GITHUBPAGES){
+        if(linkName==""){
+            return `/SIM-VICUS-Website`
+        }
         return `/SIM-VICUS-Website/${linkName}`
     }
     return `/${linkName}`
