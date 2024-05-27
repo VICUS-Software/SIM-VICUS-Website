dab8c1afa843e6f2a06c54b3a51e8df4bae819b6
Paul <72733450+paul1995tu@users.noreply.github.com>
2024-05-27 18:16:22 +0200

    fix pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
-const isGithubpages= import.meta.env.GITHUBPAGES |process.env.GITHUBPAGES;
+const isGithubpages= import.meta.env.PROD
 
-// https://astro.build/config
+// https://astro.build/con
 export default defineConfig({
   integrations: [solidJs()],
   site:"https://vicus-software.github.io",
