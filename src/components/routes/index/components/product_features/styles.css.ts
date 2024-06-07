3d7ada0cade1a2dc2e1158d1fa99153aafa3f8c2
Paul <72733450+paul1995tu@users.noreply.github.com>
2024-06-07 16:45:36 +0200

    add basic implementation of product features

--- src/components/routes/index/components/product_features/styles.css.ts
+++ src/components/routes/index/components/product_features/styles.css.ts
@@ -1,12 +1,32 @@
 import { vars } from "../../../../../styles/theme.css.ts";
-import { style } from "@vanilla-extract/css";
+import {keyframes, style} from "@vanilla-extract/css";
+const web_kit_slide_in =keyframes( {
+    "from": {
+        opacity: "0",
+        WebkitTransform: "translate(-100%)",
+        transform: "translate(-100%)",
+        background: "transparent",
+    },
+    "to": {
+        opacity: "1",
+    },
+});
 
+const slide_in = keyframes( {
+    "from": {
+        opacity: "0",
+        WebkitTransform: "translate(-100%)",
+        transform: "translate(-100%)",
+    },
+    "to": {
+        opacity: "1",
+    },
+});
 export const feature_container = style({
     display: "grid",
     gridTemplateRows: "1fr",
-    gridTemplateColumns: "1fr 1fr",
+    gridTemplateColumns: "1.5fr 1fr 1fr",
     gap: "1rem",
-    placeContent: "center",
 });
 
 export const svg = style({
@@ -16,12 +36,35 @@ export const svg = style({
     maxHeight: "450px",
     maxWidth: "399px",
     strokeWidth: 0.1,
-   opacity:0.5,
+   opacity:1,
     marginLeft: "auto",
-    marginRight: "auto",
+    WebkitAnimation: `${web_kit_slide_in} 1s ease-out`,
+    animation: `${slide_in} 1s ease-out`,
 });
 
-export const element = style({
+export const placeHolder = style({
+    width: "49%",
+    height: "385px",
+    maxHeight: "450px",
+    maxWidth: "399px",
+
+})
+
+export const mid = style({
+    border: "1px solid red",
+    display: "grid",
+    alignItems: "center",
+
+});
+
+export const right =  style({
+    border: "1px solid red",
+    display: "grid",
+    alignItems: "center",
+
+});
+
+export const left =  style({
     border: "1px solid red",
     display: "grid",
     alignItems: "center",
@@ -29,16 +72,34 @@ export const element = style({
 });
 
 
+
 export const text = style({
     fontFamily: vars.fontFamily.bold,
     color: vars.color.text,
     textAlign:"center",
     textWrap:"wrap",
-    textIndent:"2rem"
+    textIndent:"2rem",
+    padding:"1rem"
+
+
 });
 
 
 export const heading = style({
     fontFamily: vars.fontFamily.heading,
-    color:vars.color.heading
+    color:vars.color.heading,
+    marginLeft:"auto",
+    marginRight:"auto",
+    textAlign:"center"
 })
+
+
+
+
+
+export const slideIn = style({
+    WebkitAnimation: `${web_kit_slide_in} 1s ease-out`,
+    animation: `${slide_in} 1s ease-out`,
+});
+
+
